516246adc81a1ac39a09f32c64c81b8be13a4984
mazikai <mazemzk@outlook.com>
2024-02-08 23:01:01 +0800

    demo2 推荐 SseEmitter

--- src/main/java/com/example/ssedemo/HelloController.java
+++ src/main/java/com/example/ssedemo/HelloController.java
@@ -4,9 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,11 +15,16 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
-@Controller
+@RestController
 public class HelloController {
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private HelloService helloService;
+
     @RequestMapping(value = "sse")
     @ResponseBody
     public String getStreamDataImprove(HttpServletResponse httpServletResponse) throws JsonProcessingException {
@@ -27,8 +33,8 @@ public class HelloController {
         String s = "";
         while (true) {
             Map<String, Object> map = new HashMap<>();
-            map.put("a",1);
-            map.put("b",LocalDateTime.now());
+            map.put("a", 1);
+            map.put("b", LocalDateTime.now());
             String json = objectMapper.writeValueAsString(map);
             s = "data: " + json + "\n\n";
             try {
@@ -50,12 +56,33 @@ public class HelloController {
         // 未来的两秒钟没有收到新消息，客户端每两秒就向服务器建立连接
         String s = "retry: 2000\n";
         Map<String, Object> map = new HashMap<>();
-        map.put("a",1);
-        map.put("b",LocalDateTime.now());
+        map.put("a", 1);
+        map.put("b", LocalDateTime.now());
         String json = objectMapper.writeValueAsString(map);
         s += "data: " + json + "\n\n";
         PrintWriter pw = httpServletResponse.getWriter();
         pw.write(s);
         pw.flush();
     }
+
+    @GetMapping(value = "test/{clientId}", produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
+    public SseEmitter test(@PathVariable("clientId") String clientId) {
+        final SseEmitter emitter = helloService.getConn(clientId);
+        CompletableFuture.runAsync(() -> {
+            try {
+                helloService.send(clientId);
+            } catch (Exception e) {
+                throw new RuntimeException("推送数据异常");
+            }
+        });
+
+        return emitter;
+    }
+
+//    @GetMapping("closeConn/{clientId}")
+//    public Result<String> closeConn(@PathVariable("clientId") @ApiParam("客户端 id") String clientId) {
+//        service.closeConn(clientId);
+//        return Result.success("连接已关闭");
+//    }
+
 }
---
+++ src/main/java/com/example/ssedemo/SseEmitterController.java
@@ -0,0 +1,53 @@
+package com.example.ssedemo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sse")
+public class SseEmitterController {
+
+    /**
+     * 用于创建连接
+     */
+    @GetMapping("/connect/{userId}")
+    public SseEmitter connect(@PathVariable String userId) {
+        return SseEmitterServer.connect(userId);
+    }
+
+    @GetMapping("/close/{userId}")
+    public void close(@PathVariable String userId) {
+        SseEmitterServer.removeUser(userId);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<String>> list() {
+        return ResponseEntity.ok(SseEmitterServer.getIds());
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Integer> getUserCount() {
+        return ResponseEntity.ok(SseEmitterServer.getUserCount());
+    }
+
+    @GetMapping("/push/{message}")
+    public ResponseEntity<String> push(@PathVariable(name = "message") String message) {
+        SseEmitterServer.batchSendMessage(message);
+        return ResponseEntity.ok("WebSocket 推送消息给所有人");
+    }
+
+    @GetMapping("/pushTag/{userId}/{message}")
+    public ResponseEntity<String> pushTag(@PathVariable(name = "userId") String userId,
+                                          @PathVariable(name = "message") String message) {
+        SseEmitterServer.sendMessage(userId, message);
+        return ResponseEntity.ok("WebSocket 推送消息给：" + userId);
+    }
+
+
+}
